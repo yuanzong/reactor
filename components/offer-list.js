c9ab8edc0b2874fc782e37844e45da37fd8f5b50
Yuanzong Qiu <yuanzong@uber.com>
2015-05-19 14:15:56 -0700

    better layout

--- components/offer-list.js
+++ components/offer-list.js
@@ -1,10 +1,12 @@
 'use strict';
 
+var moment = require('moment');
 var React = require('react-native');
 
 var { ListView, PropTypes, Text, Image, View } = React;
 
 var styles = require('./styles');
+var offerType = require('../offer-type');
 
 module.exports = React.createClass({
   props: {
@@ -12,12 +14,25 @@ module.exports = React.createClass({
   },
 
   renderRow: function (row) {
+    var offer = row.offer;
     return (
-      <Image
-        source={{uri: row.image}}
-        style={styles.offerContainer}>
-        <Text>{row.name}</Text>
-      </Image>
+      <View style={[styles.offerContainer, styles.rounded]}>
+        <Image
+          source={{uri: row.image}}
+          style={[styles.offerImage, styles.rounded]}>
+          <Text style={styles.offerBannerText}>
+            {row.name}
+          </Text>
+          <View style={styles.offerBadge}>
+            <Text style={styles.offerBadgeText}>
+              {offer.free ? 'Ride on us!' : 'Save $' + offer.credit}
+            </Text>
+          </View>
+        </Image>
+        <View style={styles.offerDetail}>
+          <Text>{row.name}</Text>
+        </View>
+      </View>
     );
   },
 
--- components/styles.js
+++ components/styles.js
@@ -2,16 +2,68 @@
 
 var { StyleSheet } = require('react-native');
 
+var black = '#000000';
+var blue = '#1EACC7';
+var white = '#FFFFFF';
+var green = '#27AA0B';
+var orange = '#F32F00';
+var yellow = '#FFC000';
+
 module.exports = StyleSheet.create({
+  // shared
+  rounded: {
+    borderRadius: 2
+  },
+
   appContainer: {
-    backgroundColor: 'white'
+    backgroundColor: white,
+    padding: 2
   },
 
   offerContainer: {
     margin: 2,
-    borderRadius: 3,
-    borderWidth: 1,
-    borderColor: 'black',
-    height: 120
+    shadowColor: black,
+    shadowOpacity: 0.8,
+    shadowRadius: 1,
+    shadowOffset: {
+      height: 1,
+      width: 0
+    }
+  },
+
+  offerImage: {
+    height: 140,
+    resizeMode: 'cover'
+  },
+
+  offerBannerText: {
+    containerBackgroundColor: black,
+    opacity: 0.7,
+    color: white,
+    padding: 5,
+    fontSize: 11
+  },
+
+  offerBadge: {
+    marginTop: 90,
+    flexDirection: 'row',
+    justifyContent: 'flex-end',
+    backgroundColor: 'transparent'
+  },
+
+  offerBadgeText: {
+    paddingTop: 3,
+    paddingBottom: 3,
+    paddingLeft: 6,
+    paddingRight: 6,
+    containerBackgroundColor: orange,
+    color: white,
+    fontSize: 8,
+    fontWeight: 'bold'
+  },
+
+  offerDetail: {
+    backgroundColor: white,
+    padding: 10
   }
 });
